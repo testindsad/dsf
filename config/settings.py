da9e2ab3f416c09403cd09634b733d1acbaa7a62
testindsad <dekoderabia@gmail.com>
2023-05-20 07:26:09 -0700

    test

--- config/settings.py
+++ config/settings.py
@@ -130,9 +130,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 
-MEDIA_URL = '/media/'
+MEDIA_URL = 'media/'
 
   
 STATIC_ROOT = '/home/asanodenis.biz/public_html/dsf/asanpay/static'
